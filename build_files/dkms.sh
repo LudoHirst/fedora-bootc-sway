991156fb878af2857f2e685250001a6c416f4338
Ludo Hirst <oc22569@bristol.ac.uk>
2026-06-12 11:32:48 +0100

    Update dkms.sh

--- build_files/dkms.sh
+++ build_files/dkms.sh
@@ -1,4 +1,3 @@
-mkdir /usr/src/macbook12-bluetooth-1.0
 git clone https://github.com/leifliddy/macbook12-bluetooth-driver.git /usr/src/macbook12-bluetooth-1.0
 dkms add -m macbook12-bluetooth -v 1.0
 dkms build -m macbook12-bluetooth -v 1.0
