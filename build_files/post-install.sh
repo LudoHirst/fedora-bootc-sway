8b7d976827289f25eab34455f44ddb4e7cd426b4
Ludo Hirst <oc22569@bristol.ac.uk>
2026-06-12 15:59:18 +0100

    Update post-install.sh

--- build_files/post-install.sh
+++ build_files/post-install.sh
@@ -4,6 +4,7 @@ set -ouex pipefail
 systemctl enable macbook-nvme-fix.service
 systemctl enable greetd.service
 systemctl set-default graphical.target
+systemctl mask systemd-remount-fs.service
 
 # remove softwares
 dnf remove -y \
